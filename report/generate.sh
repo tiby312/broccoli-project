2e01d24752d0c4423153fb767c537667bad21747
Ken Reed <kenakioreed@gmail.com>
2020-10-16 17:20:18 -0400

    c

--- report/generate.sh
+++ report/generate.sh
@@ -3,8 +3,9 @@ set -ex
 cargo build --release --manifest-path data_gen/Cargo.toml
 
 ../target/release/data_gen bench src/raw
-../target/release/data_gen theory src/raw
+#../target/release/data_gen theory src/raw
 ../target/release/data_gen graph src/raw src/graphs
 
-xdg-open http://localhost:3000/ &
-mdbook serve
+#xdg-open http://localhost:3000/ &
+#mdbook serve
+xdg-open book/index.html
