69eb8f00133f9b159613530d3d1733a3476affe4
Ken Reed <kenakioreed@gmail.com>
2020-10-22 20:40:26 -0400

    cleanup

--- report/data_gen/src/colfind/height_heur_comparison.rs
+++ report/data_gen/src/colfind/height_heur_comparison.rs
@@ -14,7 +14,8 @@ pub fn handle_bench_inner(scene: &mut bot::BotScene<Bot>, height: usize) -> f64 
         let mut bb = bbox_helper::create_bbox_mut(bots, |b| prop.create_bbox_i32(b.pos));
     
         let mut tree = TreeBuilder::new(&mut bb).with_height(height).build_seq();
-    
+        assert_eq!(tree.get_height(),height);
+
         tree.find_colliding_pairs_mut(|a, b| {
             a.num += 2;
             b.num += 2;
@@ -33,7 +34,8 @@ pub fn handle_theory_inner(scene: &mut bot::BotScene<Bot>, height: usize) -> usi
             maker.from_rect( prop.create_bbox_i32(b.pos))
         });
         let mut tree = TreeBuilder::new(&mut bb).with_height(height).build_seq();
-    
+        assert_eq!(tree.get_height(),height);
+
         tree.find_colliding_pairs_mut(|a, b| {
             a.num += 2;
             b.num += 2;
@@ -52,20 +54,11 @@ fn handle_lowest(fb: &mut FigureBuilder) {
         num_bots: usize,
     }
 
-    /*
-    struct TheoryRecord {
-        height: usize,
-        num_bots: usize,
-    }
-    */
-
     let mut benches: Vec<BenchRecord> = Vec::new();
-    //let mut theories: Vec<TheoryRecord> = Vec::new();
-
+    
     let its = (1usize..80_000).step_by(2000);
     for num_bots in its.clone() {
         let mut minimum = None;
-        //let mut minimum_theory = None;
         let max_height = (num_bots as f64).log2() as usize;
 
         let mut scene = bot::BotSceneBuilder::new(num_bots).build_specialized(|_, pos| Bot {
@@ -74,7 +67,6 @@ fn handle_lowest(fb: &mut FigureBuilder) {
         });
 
         for height in 1..max_height {
-            //let theory = handle_theory_inner(&mut scene, height);
             let bench = handle_bench_inner(&mut scene, height);
             match minimum {
                 Some((a, _b)) => {
@@ -86,27 +78,9 @@ fn handle_lowest(fb: &mut FigureBuilder) {
                     minimum = Some((bench, height));
                 }
             }
-            /*
-            match minimum_theory {
-                Some((a, _b)) => {
-                    if theory < a {
-                        minimum_theory = Some((theory, height));
-                    }
-                }
-                None => {
-                    minimum_theory = Some((theory, height));
-                }
-            }
-            */
+            
         }
 
-        /*
-        if let Some((_, height)) = minimum_theory {
-            theories.push(TheoryRecord { height, num_bots });
-            let (_, height) = minimum.unwrap();
-            benches.push(BenchRecord { height, num_bots });
-        }
-        */
         if let Some((_, height)) = minimum {
             benches.push(BenchRecord { height, num_bots });
         }
@@ -114,9 +88,6 @@ fn handle_lowest(fb: &mut FigureBuilder) {
 
     {
         let mut fg = fb.build("height_heuristic_vs_optimal");
-
-        //let xx = theories.iter().map(|a| a.num_bots);
-        //let yy = theories.iter().map(|a| a.height);
 
         let x = benches.iter().map(|a| a.num_bots);
         let y = benches.iter().map(|a| a.height);
@@ -132,45 +103,7 @@ fn handle_lowest(fb: &mut FigureBuilder) {
 
         let heurx = heur.iter().map(|a| a.0);
         let heury = heur.iter().map(|a| a.1);
-
-        /*
         fg.axes2d()
-            .set_pos_grid(2, 1, 0)
-            .set_legend(
-                Graph(1.0),
-                Graph(0.0),
-                &[LegendOption::Placement(AlignRight, AlignBottom)],
-                &[],
-            )
-            .set_title(
-                "Dinotree Colfind Query: Optimal Height vs Heuristic Height with abspiral(x,2.0)",
-                &[],
-            )
-            .set_x_label("Num bots", &[])
-            .set_y_label("Best Tree Height", &[])
-            .points(
-                xx,
-                yy,
-                &[
-                    Caption("Optimal"),
-                    PointSymbol('O'),
-                    Color("red"),
-                    PointSize(1.0),
-                ],
-            )
-            .points(
-                heurx.clone(),
-                heury.clone(),
-                &[
-                    Caption("Heuristic"),
-                    PointSymbol('x'),
-                    Color("blue"),
-                    PointSize(2.0),
-                ],
-            );
-        */
-        fg.axes2d()
-            //.set_pos_grid(2, 1, 1)
             .set_legend(
                 Graph(1.0),
                 Graph(0.0),
