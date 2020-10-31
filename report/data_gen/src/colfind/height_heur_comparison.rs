0c44d5e1f1fc90d0edc1de53591508dd66eafd07
Ken Reed <kenakioreed@gmail.com>
2020-10-30 22:12:25 -0400

    c

--- report/data_gen/src/colfind/height_heur_comparison.rs
+++ report/data_gen/src/colfind/height_heur_comparison.rs
@@ -59,6 +59,7 @@ fn handle_lowest(fb: &mut FigureBuilder) {
         let mut minimum = None;
         let max_height = (num_bots as f64).log2() as usize;
 
+        
         let mut scene = bot::BotSceneBuilder::new(num_bots).build_specialized(|_, pos| Bot {
             pos: pos.inner_as(),
             num: 0,
