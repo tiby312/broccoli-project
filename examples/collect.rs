0585c522085ecdcc8c0effefcb6e66b6e3cf988a
Ken <kenakioreed@gmail.com>
2020-11-21 06:44:49 -0500

    docs

--- examples/collect.rs
+++ examples/collect.rs
@@ -7,8 +7,6 @@ fn main() {
         broccoli::bbox(broccoli::rect(5, 15, 5, 15), 2),
     ];
 
-    //This will change the order of the elements in bboxes,
-    //but this is okay since we populated it with mutable references.
     let mut tree = broccoli::collections::TreeRefInd::new(&mut aabbs,|a|{
         a.rect
     });
@@ -31,7 +29,7 @@ fn main() {
 
     //We can iterate over all the colliding pairs as well as our custom group
     //multiple times without having to query the tree over and over again.
-    for _ in 0..3{
+    for _ in 0..2{
         //mutate our custom group
         for (a,()) in evens.get_mut(&mut aabbs).iter_mut(){
             a.inner+=1;
@@ -42,7 +40,9 @@ fn main() {
             a.inner+=1;
             b.inner+=1;
         })
-
     }
 
+    assert_eq!(aabbs[0].inner,3);
+    assert_eq!(aabbs[1].inner,1);
+    assert_eq!(aabbs[2].inner,5);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -20,8 +20,9 @@
 //! The [`collections`] module goes into more depth as well as the book mentioned above.
 //!
 //! TL;DR use [`Tree`] and fill it with `BBox<u32,&mut T>` unless you want
-//! to use functions like [`collect_colliding_pairs`](crate::collections::TreeRefInd::collect_colliding_pairs). See
-//! the github [examples](https://github.com/tiby312/broccoli/tree/master/examples).
+//! to use functions like [`collect_colliding_pairs`](crate::collections::TreeRefInd::collect_colliding_pairs). 
+//!
+//! Checkout the github [examples](https://github.com/tiby312/broccoli/tree/master/examples).
 //!
 //! ### Parallelism
 //!
