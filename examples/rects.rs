fbec43c11820e5b0b882e2e0eb59e45d95aa68dd
Ken <kenakioreed@gmail.com>
2020-11-22 10:26:03 -0500

    formatting

--- examples/rects.rs
+++ examples/rects.rs
@@ -1,10 +1,9 @@
-use axgeom::vec2;
 use broccoli::{bbox, prelude::*, rect};
 
 fn main() {
-    let mut inner1 = vec2(5, 5);
-    let mut inner2 = vec2(3, 3);
-    let mut inner3 = vec2(7, 7);
+    let mut inner1 = 4;
+    let mut inner2 = 5;
+    let mut inner3 = 6;
 
     let mut bots = [
         bbox(rect(0isize, 10, 0, 10), &mut inner1),
@@ -12,7 +11,7 @@ fn main() {
         bbox(rect(5, 15, 5, 15), &mut inner3),
     ];
 
-    let mut tree = broccoli::collections::TreeRef::new(&mut bots);
+    let tree = broccoli::collections::TreeRef::new(&mut bots);
 
     //Here we query for read-only references so we can pull
     //them out of the closure.
@@ -23,4 +22,5 @@ fn main() {
 
     assert_eq!(rect_collisions.len(), 1);
     assert_eq!(*rect_collisions[0].get(), rect(0, 10, 0, 10));
+    assert_eq!(*rect_collisions[0].inner, 4);
 }
