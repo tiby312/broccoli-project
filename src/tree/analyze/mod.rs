9f082b3abb8646e3cb24f182005de571996d0445
Ken <kenakioreed@gmail.com>
2020-12-08 20:56:21 -0500

    height fix

--- src/tree/analyze/mod.rs
+++ src/tree/analyze/mod.rs
@@ -141,16 +141,12 @@ const fn compute_tree_height_heuristic(num_bots: usize, num_per_node: usize) -> 
     if num_bots <= num_per_node {
         1
     } else {
-        /*
-        let a = num_bots as f32 / num_per_node as f32;
-        let b = a.log2() / 2.0;
-        (b.ceil() as usize) * 2 + 1
-        */
+
         let (num_bots,num_per_node) = (num_bots as u64,num_per_node as u64);
         let a = num_bots / num_per_node;
         let a = log_2(a) ;
         //let ans=(a/2)*2+1;
-        a as usize
+        (a+1) as usize
 
     }
 }
