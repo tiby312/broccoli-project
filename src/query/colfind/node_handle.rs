0011d115770f7e730d14c66a6aa8da7b36814ea5
Ken <kenakioreed@gmail.com>
2020-12-12 14:28:59 -0500

    swapped arguments and its faster

--- src/query/colfind/node_handle.rs
+++ src/query/colfind/node_handle.rs
@@ -183,10 +183,19 @@ impl<K: ColMulti + Splitter> NodeHandler for HandleSorted<K> {
 
             oned::find_perp_2d1(current.axis, r1, r2, func);
         } else if current.cont().intersects(anchor.cont()) {
+            /*
             oned::find_parallel_2d(
                 current.axis.next(),
                 current.node.into_range(),
                 anchor.node.borrow_mut().into_range(),
+                func,
+            );
+            */
+            //This is faster than above.
+            oned::find_parallel_2d(
+                current.axis,
+                anchor.node.borrow_mut().into_range(),
+                current.node.into_range(),
                 func,
             );
         }
