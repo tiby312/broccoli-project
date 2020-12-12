96fff4767bbf7eefe46ba449c4e08d49dd1520ba
Ken <kenakioreed@gmail.com>
2020-12-12 08:30:33 -0500

    order change

--- report/data_gen/src/main.rs
+++ report/data_gen/src/main.rs
@@ -141,7 +141,7 @@ fn main() {
             let path = Path::new(folder.trim_end_matches('/'));
             std::fs::create_dir_all(&path).expect("failed to create directory");
             let mut fb = FigureBuilder::new(folder);
-
+            
             run_test!(&mut fb, colfind::query_evenness::handle_theory);
 
             run_test!(&mut fb, colfind::level_analysis::handle_theory);
@@ -153,6 +153,7 @@ fn main() {
             run_test!(&mut fb, colfind::construction_vs_query::handle_theory);
 
             run_test!(&mut fb, colfind::theory_colfind_3d::handle);
+            
         }
         "bench" => {
             let folder = args[2].clone();
@@ -161,13 +162,15 @@ fn main() {
             let mut fb = FigureBuilder::new(folder);
 
             //done
+            run_test!(&mut fb, colfind::rebal_strat::handle);
+            
+            
             run_test!(&mut fb, colfind::construction_vs_query::handle_bench);
 
             run_test!(&mut fb, colfind::level_analysis::handle_bench);
 
             run_test!(&mut fb, colfind::colfind::handle_bench);
 
-            run_test!(&mut fb, colfind::rebal_strat::handle);
             run_test!(&mut fb, colfind::dinotree_direct_indirect::handle);
 
             run_test!(&mut fb, colfind::float_vs_integer::handle);
@@ -175,7 +178,7 @@ fn main() {
             //This is the one thats interesting to see what the results are on phone/vs/laptop
             run_test!(&mut fb, colfind::parallel_heur_comparison::handle);
             run_test!(&mut fb, colfind::height_heur_comparison::handle);
-
+            
             //nbody::theory::handle(&mut fb);
         }
         "graph" => {
