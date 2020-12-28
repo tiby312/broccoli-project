0ef74bfbcc55039cc48c48b21e9584168f838955
Ken <kenakioreed@gmail.com>
2020-12-27 19:38:11 -0500

    removed naive trait

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -11,39 +11,6 @@ mod inner_prelude {
 }
 
 
-///Provides the naive implementation of the [`Tree`] api.
-pub struct NaiveAlgs<'a, T> {
-    bots: PMut<'a, [T]>,
-}
-
-
-impl<'a,T:Aabb> NaiveQueries for NaiveAlgs<'a,T>{
-    type T=T;
-    type Num=T::Num;
-    fn get_slice_mut(&mut self)->PMut<[T]>{
-        self.bots.borrow_mut()
-    }
-
-}
-
-
-
-impl<'a, T: Aabb> NaiveAlgs<'a, T> {
-    #[must_use]
-    pub fn from_slice(a: &'a mut [T]) -> NaiveAlgs<'a, T> {
-        NaiveAlgs { bots: PMut::new(a) }
-    }
-    #[must_use]
-    pub fn new(bots: PMut<'a, [T]>) -> NaiveAlgs<'a, T> {
-        NaiveAlgs { bots }
-    }
-
-    //#[cfg(feature = "nbody")]
-    pub fn nbody(&mut self, func: impl FnMut(PMut<T>, PMut<T>)) {
-        nbody::naive_mut(self.bots.borrow_mut(), func);
-    }
-}
-
 
 
 ///aabb broadphase collision detection
@@ -71,11 +38,6 @@ pub mod rect;
 mod tools;
 
 use self::inner_prelude::*;
-pub trait NaiveQueries{
-    type T:Aabb<Num=Self::Num>;
-    type Num:Num;
-    fn get_slice_mut(&mut self) -> PMut<[Self::T]>;
-}
 
 
 pub trait NaiveComparable<'a>{
