54751d78584cfbd4aacfff014018beb79a1dfd2d
Ken <kenakioreed@gmail.com>
2021-01-01 18:46:16 -0500

    query cleaup

--- src/query/colfind/inner.rs
+++ src/query/colfind/inner.rs
@@ -85,67 +85,39 @@ where
         m: VistrMut<Node<T>>,
         splitter: &mut K,
     ) {
-        let (mut nn, rest) = m.next();
+        if let Some([left,right])=Self::recurse_common(this_axis,sweeper,m){
+            let (mut splitter11, mut splitter22) = splitter.div();
+            match par.next() {
+                par::ParResult::Parallel([dleft, dright]) => {
+                    let (mut sweeper1, mut sweeper2) = sweeper.div();
+                    
+                    rayon::join(|| 
+                        self.recurse_par(
+                            this_axis.next(),
+                            dleft,
+                            &mut sweeper1,
+                            left,
+                            &mut splitter11,
+                        )
+                    , ||self.recurse_par(
+                        this_axis.next(),
+                        dright,
+                        &mut sweeper2,
+                        right,
+                        &mut splitter22,
+                    ));
 
-        match rest {
-            Some([mut left, mut right]) => {
-                let (mut splitter11, mut splitter22) = splitter.div();
-
-                //Continue to recurse even if we know there are no more bots
-                //This simplifies query algorithms that might be building up
-                //a tree.
-                if nn.div.is_some() {
-                    sweeper.handle_node(this_axis.next(), nn.borrow_mut().into_range());
-
-                    if let Some(nn) = DestructuredNode::new(this_axis, nn) {
-                        let left = left.borrow_mut();
-                        let right = right.borrow_mut();
-                        let mut g = InnerRecurser::new(nn, sweeper);
-                        g.recurse(this_axis.next(), left);
-                        g.recurse(this_axis.next(), right);
-                    }
-
-                    match par.next() {
-                        par::ParResult::Parallel([dleft, dright]) => {
-                            let (mut sweeper1, mut sweeper2) = sweeper.div();
-                            let (splitter11ref, splitter22ref) = (&mut splitter11, &mut splitter22);
-                            let (sweeper11ref, sweeper22ref) = (&mut sweeper1, &mut sweeper2);
-
-                            let af = move || {
-                                self.recurse_par(
-                                    this_axis.next(),
-                                    dleft,
-                                    sweeper11ref,
-                                    left,
-                                    splitter11ref,
-                                )
-                            };
-                            let bf = move || {
-                                self.recurse_par(
-                                    this_axis.next(),
-                                    dright,
-                                    sweeper22ref,
-                                    right,
-                                    splitter22ref,
-                                )
-                            };
-                            rayon::join(af, bf);
-
-                            sweeper.add(sweeper1, sweeper2);
-                        }
-                        par::ParResult::Sequential(_) => {
-                            self.recurse_seq(this_axis.next(), sweeper, left, &mut splitter11);
-                            self.recurse_seq(this_axis.next(), sweeper, right, &mut splitter22);
-                        }
-                    }
+                    sweeper.add(sweeper1, sweeper2);
                 }
+                par::ParResult::Sequential(_) => {
+                    self.recurse_seq(this_axis.next(), sweeper, left, &mut splitter11);
+                    self.recurse_seq(this_axis.next(), sweeper, right, &mut splitter22);
+                }
+            }
 
-                splitter.add(splitter11, splitter22);
-            }
-            None => {
-                sweeper.handle_node(this_axis.next(), nn.into_range());
-            }
+            splitter.add(splitter11, splitter22);
         }
+        
     }
 }
 
@@ -155,19 +127,13 @@ impl<T: Aabb, K: Splitter, S: NodeHandler<T = T> + Splitter> ColFindRecurser<T, 
         ColFindRecurser { _p: PhantomData }
     }
 
-    pub fn recurse_seq<A: Axis>(
-        &self,
-        this_axis: A,
-        sweeper: &mut S,
-        m: VistrMut<Node<T>>,
-        splitter: &mut K,
-    ) {
+    pub fn recurse_common<'a,'b,A:Axis>(this_axis:A,sweeper:&mut S,m:VistrMut<'b,Node<'a,T>>)
+        ->Option<[VistrMut<'b,Node<'a,T>>;2]>{
+        
         let (mut nn, rest) = m.next();
-        //let mut nn = nn.get_mut();
-
+    
         match rest {
             Some([mut left, mut right]) => {
-                let (mut splitter11, mut splitter22) = splitter.div();
 
                 //Continue to recurse even if we know there are no more bots
                 //This simplifies query algorithms that might be building up
@@ -184,14 +150,30 @@ impl<T: Aabb, K: Splitter, S: NodeHandler<T = T> + Splitter> ColFindRecurser<T, 
                     }
                 }
 
-                self.recurse_seq(this_axis.next(), sweeper, left, &mut splitter11);
-                self.recurse_seq(this_axis.next(), sweeper, right, &mut splitter22);
-
-                splitter.add(splitter11, splitter22);
+                Some([left,right])
             }
             None => {
                 sweeper.handle_node(this_axis.next(), nn.into_range());
+                None
             }
+        }
+    }
+
+    pub fn recurse_seq<A: Axis>(
+        &self,
+        this_axis: A,
+        sweeper: &mut S,
+        m: VistrMut<Node<T>>,
+        splitter: &mut K,
+    ) {
+
+        if let Some([left,right])=Self::recurse_common(this_axis,sweeper,m){
+            let (mut splitter11, mut splitter22) = splitter.div();
+            self.recurse_seq(this_axis.next(), sweeper, left, &mut splitter11);
+            self.recurse_seq(this_axis.next(), sweeper, right, &mut splitter22);
+
+            splitter.add(splitter11, splitter22);
+
         }
     }
 }
