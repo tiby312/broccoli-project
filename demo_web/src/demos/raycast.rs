7372278fc4987fe9778577fb80bbaf6f84205076
Ken Reed <kenakioreed@gmail.com>
2022-01-01 13:11:51 -0500

    newv

--- demo_web/src/demos/raycast.rs
+++ demo_web/src/demos/raycast.rs
@@ -78,6 +78,8 @@ pub fn make_demo(dim: Rect<f32>, ctx: &web_sys::WebGl2RenderingContext) -> Demo 
         ctx.clear_color(0.13, 0.13, 0.13, 1.0);
         ctx.clear(web_sys::WebGl2RenderingContext::COLOR_BUFFER_BIT);
 
+
+        
         sys.draw_circles(
             &circle_save,
             vec2(dim.x.end, dim.y.end),
