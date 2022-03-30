48af56454a166872aa090e681c6f9c1c187fc21b
Ken Reed <kenakioreed@gmail.com>
2022-03-30 11:25:08 -0400

    restore test

--- tests/broccoli_test.rs
+++ tests/broccoli_test.rs
@@ -2,8 +2,7 @@ use axgeom;
 use broccoli::*;
 #[test]
 fn test1() {
-    //0, 20,/*  100, 200*/
-    for &num_bots in [5].iter() {
+    for &num_bots in [0,20,100,200].iter() {
         let s = dists::spiral_iter([400.0, 400.0], 12.0, 1.0);
 
         let mut bots: Vec<_> = s
