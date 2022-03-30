df1172fbec9d7715e14e96abc0344a952a108a77
Ken Reed <kenakioreed@gmail.com>
2022-03-30 12:21:30 -0400

    better miri test

--- tests/broccoli_test.rs
+++ tests/broccoli_test.rs
@@ -5,22 +5,36 @@ fn test1() {
     for &num_bots in [0, 20, 100, 200].iter() {
         let s = dists::spiral_iter([400.0, 400.0], 12.0, 1.0);
 
+        let mut data: Vec<_> = (0..num_bots).collect();
+
         let mut bots: Vec<_> = s
             .take(num_bots)
-            .enumerate()
-            .map(|(_, [x, y])| {
+            .zip(data.iter_mut())
+            .map(|([x, y], dd)| {
                 bbox(
                     axgeom::Rect::from_point(
                         axgeom::vec2(x as i64, y as i64),
                         axgeom::vec2same(8 as i64),
                     )
                     .into(),
-                    (),
+                    dd,
                 )
             })
             .collect();
 
-        //let tree = broccoli::new(&mut bots);
+        let mut bots: Vec<_> = bots.iter_mut().collect();
+
+        let mut tree = broccoli::new(&mut bots);
+        let mut prevec = broccoli::util::PreVec::new();
+
+        tree.colliding_pairs()
+            .recurse_seq(&mut prevec, &mut |a, b| {
+                let a = a.unpack_inner();
+                let b = b.unpack_inner();
+                let sum = **a + **b;
+                **a ^= sum;
+                **b ^= sum;
+            });
         //broccoli::queries::assert_tree_invariants(&tree);
         broccoli::queries::colfind::assert_query(&mut bots);
     }
