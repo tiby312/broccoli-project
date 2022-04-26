e2dda4977c819ae11501e0c597bf88ff6b82bd0b
Ken Reed <kenakioreed@gmail.com>
2022-04-26 14:16:33 -0400

    simplify

--- src/queries/colfind/build.rs
+++ src/queries/colfind/build.rs
@@ -28,10 +28,7 @@ pub struct NodeFinisher<'b, T> {
 }
 impl<'b, T: Aabb> NodeFinisher<'b, T> {
     pub fn finish<H: NodeHandler<T>>(self, handler: &mut H) {
-        match self.axis {
-            AxisDyn::X => handler.handle_node(axgeom::XAXIS.next(), self.bots, self.is_leaf),
-            AxisDyn::Y => handler.handle_node(axgeom::YAXIS.next(), self.bots, self.is_leaf),
-        }
+        handler.handle_node(self.axis, self.bots, self.is_leaf);
     }
 }
 
@@ -70,7 +67,8 @@ impl<'a, 'b, T: Aabb> CollVis<'a, 'b, T> {
 
             if let Some([mut left, mut right]) = rest {
                 struct InnerRecurser<'a, 'node, T: Aabb, NN> {
-                    anchor: NodeAxis<'a, 'node, T>,
+                    anchor: AabbPin<&'a mut Node<'node, T>>,
+                    anchor_axis: AxisDyn,
                     handler: &'a mut NN,
                 }
 
@@ -81,22 +79,21 @@ impl<'a, 'b, T: Aabb> CollVis<'a, 'b, T> {
                     fn recurse(
                         &mut self,
                         this_axis: AxisDyn,
-                        m: VistrMutPin<Node<T>>,
+                        m: VistrMutPin<Node<'node, T>>,
                         is_left: bool,
                     ) {
-                        let anchor_axis = self.anchor.axis;
+                        let anchor_axis = self.anchor_axis;
                         let current_is_leaf = m.get_height() == 1;
 
                         let (mut nn, rest) = m.next();
 
-                        let current = NodeAxis {
-                            node: nn.borrow_mut(),
-                            axis: this_axis,
-                        };
-
                         self.handler.handle_children(
-                            self.anchor.borrow_mut(),
-                            current,
+                            Floop {
+                                anchor: self.anchor.borrow_mut(),
+                                anchor_axis: self.anchor_axis,
+                                current: nn.borrow_mut(),
+                                current_axis: this_axis,
+                            },
                             current_is_leaf,
                         );
 
@@ -105,13 +102,13 @@ impl<'a, 'b, T: Aabb> CollVis<'a, 'b, T> {
                                 if anchor_axis.is_equal_to(this_axis) {
                                     match is_left {
                                         true => {
-                                            if div < self.anchor.node.cont.start {
+                                            if div < self.anchor.cont.start {
                                                 self.recurse(this_axis.next(), right, is_left);
                                                 return;
                                             }
                                         }
                                         false => {
-                                            if div >= self.anchor.node.cont.end {
+                                            if div >= self.anchor.cont.end {
                                                 self.recurse(this_axis.next(), left, is_left);
                                                 return;
                                             }
@@ -128,10 +125,8 @@ impl<'a, 'b, T: Aabb> CollVis<'a, 'b, T> {
 
                 if nn.div.is_some() {
                     let mut g = InnerRecurser {
-                        anchor: NodeAxis {
-                            node: nn,
-                            axis: this_axis,
-                        },
+                        anchor: nn,
+                        anchor_axis: this_axis,
                         handler: data.handler,
                     };
 
@@ -192,32 +187,21 @@ impl<'a, 'b, T: Aabb> CollVis<'a, 'b, T> {
     }
 }
 
-/// Used by [`NodeHandler`]
-pub struct NodeAxis<'a, 'node, T: Aabb> {
-    pub node: AabbPin<&'a mut Node<'node, T>>,
-    pub axis: AxisDyn,
-}
-
-impl<'a, 'node, T: Aabb> NodeAxis<'a, 'node, T> {
-    #[inline(always)]
-    pub fn borrow_mut<'c>(&'c mut self) -> NodeAxis<'c, 'node, T>
-    where
-        'a: 'c,
-    {
-        NodeAxis {
-            node: self.node.borrow_mut(),
-            axis: self.axis,
-        }
-    }
+//TODO document
+pub struct Floop<'a, 'node, T: Aabb> {
+    pub anchor: AabbPin<&'a mut Node<'node, T>>,
+    pub current: AabbPin<&'a mut Node<'node, T>>,
+    pub anchor_axis: AxisDyn,
+    pub current_axis: AxisDyn,
 }
 
 ///
 /// Abstract over sorted and non sorted trees
 ///
 pub trait NodeHandler<T: Aabb> {
-    fn handle_node(&mut self, axis: impl Axis, bots: AabbPin<&mut [T]>, is_leaf: bool);
+    fn handle_node(&mut self, axis: AxisDyn, bots: AabbPin<&mut [T]>, is_leaf: bool);
 
-    fn handle_children(&mut self, anchor: NodeAxis<T>, current: NodeAxis<T>, current_is_leaf: bool);
+    fn handle_children(&mut self, floop: Floop<T>, current_is_leaf: bool);
 }
 
 #[derive(Clone)]
@@ -226,37 +210,44 @@ pub struct NoSortQuery<F> {
 }
 
 impl<T: Aabb, F: FnMut(AabbPin<&mut T>, AabbPin<&mut T>)> NodeHandler<T> for NoSortQuery<F> {
-    fn handle_node(&mut self, axis: impl Axis, bots: AabbPin<&mut [T]>, is_leaf: bool) {
-        if !is_leaf {
-            queries::for_every_pair(bots, move |a, b| {
-                if a.get().get_range(axis).intersects(b.get().get_range(axis)) {
-                    self.func.collide(a, b);
-                }
-            });
-        } else {
-            queries::for_every_pair(bots, move |a, b| {
-                if a.get().intersects_rect(b.get()) {
-                    self.func.collide(a, b);
-                }
-            });
+    fn handle_node(&mut self, axis: AxisDyn, bots: AabbPin<&mut [T]>, is_leaf: bool) {
+        fn foop<T: Aabb, F: FnMut(AabbPin<&mut T>, AabbPin<&mut T>)>(
+            func: &mut F,
+            axis: impl Axis,
+            bots: AabbPin<&mut [T]>,
+            is_leaf: bool,
+        ) {
+            if !is_leaf {
+                queries::for_every_pair(bots, move |a, b| {
+                    if a.get().get_range(axis).intersects(b.get().get_range(axis)) {
+                        func.collide(a, b);
+                    }
+                });
+            } else {
+                queries::for_every_pair(bots, move |a, b| {
+                    if a.get().intersects_rect(b.get()) {
+                        func.collide(a, b);
+                    }
+                });
+            }
+        }
+
+        match axis.next() {
+            AxisDyn::X => foop(&mut self.func, XAXIS, bots, is_leaf),
+            AxisDyn::Y => foop(&mut self.func, YAXIS, bots, is_leaf),
         }
     }
 
-    fn handle_children(
-        &mut self,
-        mut anchor: NodeAxis<T>,
-        current: NodeAxis<T>,
-        _current_is_leaf: bool,
-    ) {
-        let res = if !current.axis.is_equal_to(anchor.axis) {
+    fn handle_children(&mut self, mut f: Floop<T>, _current_is_leaf: bool) {
+        let res = if !f.current_axis.is_equal_to(f.anchor_axis) {
             true
         } else {
-            current.node.cont.intersects(&anchor.node.cont)
+            f.current.cont.intersects(&f.anchor.cont)
         };
 
         if res {
-            for mut a in current.node.into_range().iter_mut() {
-                for mut b in anchor.node.borrow_mut().into_range().iter_mut() {
+            for mut a in f.current.into_range().iter_mut() {
+                for mut b in f.anchor.borrow_mut().into_range().iter_mut() {
                     if a.get().intersects_rect(b.get()) {
                         self.func.collide(a.borrow_mut(), b.borrow_mut());
                     }
@@ -274,31 +265,41 @@ pub struct QueryDefault<F> {
 
 impl<T: Aabb, F: FnMut(AabbPin<&mut T>, AabbPin<&mut T>)> NodeHandler<T> for QueryDefault<F> {
     #[inline(always)]
-    fn handle_node(&mut self, axis: impl Axis, bots: AabbPin<&mut [T]>, is_leaf: bool) {
+    fn handle_node(&mut self, axis: AxisDyn, bots: AabbPin<&mut [T]>, is_leaf: bool) {
         //
         // All bots belonging to a non leaf node are guaranteed to touch the divider.
         // Therefore, all bots intersect along one axis already. Because:
         //
         // If a contains x and b contains x then a intersects b.
         //
-        oned::find_2d(&mut self.prevec, axis, bots, &mut self.func, is_leaf);
+        match axis.next() {
+            AxisDyn::X => oned::find_2d(
+                &mut self.prevec,
+                axgeom::XAXIS,
+                bots,
+                &mut self.func,
+                is_leaf,
+            ),
+            AxisDyn::Y => oned::find_2d(
+                &mut self.prevec,
+                axgeom::YAXIS,
+                bots,
+                &mut self.func,
+                is_leaf,
+            ),
+        }
     }
 
     #[inline(always)]
-    fn handle_children(
-        &mut self,
-        anchor: NodeAxis<T>,
-        current: NodeAxis<T>,
-        current_is_leaf: bool,
-    ) {
+    fn handle_children(&mut self, f: Floop<T>, current_is_leaf: bool) {
         use AxisDyn::*;
         let func = &mut self.func;
         let prevec = &mut self.prevec;
-        match (anchor.axis, current.axis) {
-            (X, X) => handle_parallel(XAXIS, prevec, func, anchor, current, current_is_leaf),
-            (Y, Y) => handle_parallel(YAXIS, prevec, func, anchor, current, current_is_leaf),
-            (X, Y) => handle_perp(XAXIS, func, anchor, current, current_is_leaf),
-            (Y, X) => handle_perp(YAXIS, func, anchor, current, current_is_leaf),
+        match (f.anchor_axis, f.current_axis) {
+            (X, X) => handle_parallel(XAXIS, prevec, func, f, current_is_leaf),
+            (Y, Y) => handle_parallel(YAXIS, prevec, func, f, current_is_leaf),
+            (X, Y) => handle_perp(XAXIS, func, f, current_is_leaf),
+            (Y, X) => handle_perp(YAXIS, func, f, current_is_leaf),
         }
     }
 }
@@ -306,26 +307,22 @@ impl<T: Aabb, F: FnMut(AabbPin<&mut T>, AabbPin<&mut T>)> NodeHandler<T> for Que
 fn handle_perp<T: Aabb, A: Axis>(
     axis: A,
     func: &mut impl CollisionHandler<T>,
-    mut anchor: NodeAxis<T>,
-    current: NodeAxis<T>,
+    mut f: Floop<T>,
     _current_is_leaf: bool,
 ) {
     let anchor_axis = axis;
     let current_axis = axis.next();
 
-    let cc1 = &anchor.node.cont;
-    let div = anchor.node.div.unwrap();
+    let cc1 = &f.anchor.cont;
+    let div = f.anchor.div.unwrap();
 
-    let cc2 = current.node.into_node_ref();
+    let cc2 = f.current.into_node_ref();
 
     //TODO turn into iterator so we dont do two passes
     let r1 = super::tools::get_section_mut(anchor_axis, cc2.range, cc1);
 
-    let mut r2 = super::tools::get_section_mut(
-        current_axis,
-        anchor.node.borrow_mut().into_range(),
-        cc2.cont,
-    );
+    let mut r2 =
+        super::tools::get_section_mut(current_axis, f.anchor.borrow_mut().into_range(), cc2.cont);
 
     //iterate over current nodes botd
     for y in r1.iter_mut() {
@@ -366,14 +363,13 @@ fn handle_parallel<T: Aabb, A: Axis>(
     axis: A,
     prevec: &mut PreVec,
     func: &mut impl CollisionHandler<T>,
-    anchor: NodeAxis<T>,
-    current: NodeAxis<T>,
+    f: Floop<T>,
     current_is_leaf: bool,
 ) {
-    let anchor_div = anchor.node.div.unwrap();
+    let anchor_div = f.anchor.div.unwrap();
 
-    let anchor2 = anchor.node.into_node_ref();
-    let current2 = current.node.into_node_ref();
+    let anchor2 = f.anchor.into_node_ref();
+    let current2 = f.current.into_node_ref();
 
     //TODO make this even earlier?
     let mut k = twounordered::TwoUnorderedVecs::from(prevec.extract_vec());
