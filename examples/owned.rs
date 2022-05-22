9da1f96b88353195bda0f7b41d03d5cab8336aa3
Ken Reed <kenakioreed@gmail.com>
2022-05-22 13:22:34 -0400

    better docs

--- examples/owned.rs
+++ examples/owned.rs
@@ -2,18 +2,24 @@ use broccoli::tree::{node::ManySwapBBox, rect};
 fn main() {
     let mut acc = [0; 3];
 
-    //Rect is stored directly in tree,
-    //but inner is not.
     let mut aabbs = [
         ManySwapBBox(rect(00, 10, 00, 10), 0),
         ManySwapBBox(rect(15, 20, 15, 20), 1),
         ManySwapBBox(rect(05, 15, 05, 15), 2),
     ];
 
+    // This is not lifetimed!
     let tree_data = broccoli::Tree::new(&mut aabbs).get_tree_data();
 
+    // Construct the tree again using the pre-computed tree data as
+    // as passing in the same elements (in the same order).
+    //
+    // It is the user's reponsiblity to not reorder the aabbs that
+    // were used the make the tree data.
+    let mut tree = broccoli::Tree::from_tree_data(&mut aabbs, &tree_data);
+
     //Find all colliding aabbs.
-    broccoli::Tree::from_tree_data(&mut aabbs, &tree_data).find_colliding_pairs(|a, b| {
+    tree.find_colliding_pairs(|a, b| {
         acc[a.1] += 1;
         acc[b.1] += 1;
     });
