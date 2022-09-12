c13f619cb260f4ecc96210f704341271edd4aa5e
Kenneth Reed <kenakioreed@gmail.com>
2022-09-12 19:45:33 -0400

    cleanup

--- broccoli/src/build.rs
+++ broccoli/src/build.rs
@@ -22,9 +22,7 @@ pub trait Sorter<T> {
 ///Sorts the bots based on an axis.
 #[inline(always)]
 pub fn sweeper_update<I: Aabb, A: Axis>(axis: A, collision_botids: &mut [I]) {
-    let sclosure = |a: &I, b: &I| -> core::cmp::Ordering { queries::cmp_aabb(axis, a, b) };
-
-    collision_botids.sort_unstable_by(sclosure);
+    collision_botids.sort_unstable_by(|a,b|queries::cmp_aabb(axis, a, b));
 }
 
 #[must_use]
