8b79d5cdbd199a924c6b5da89d7a6f06651fbe59
Kenneth Reed <kenakioreed@gmail.com>
2022-10-05 19:30:25 -0400

    cleanup

--- analysis/report-web/plot-gen/src/main.rs
+++ analysis/report-web/plot-gen/src/main.rs
@@ -11,8 +11,6 @@ fn foo<P: AsRef<Path>>(base: P) -> std::fmt::Result {
 
     let file = std::fs::File::create(base.join("report").with_extension("html")).unwrap();
 
-    //use tagger::no_attr;
-    //let mut w = tagger::new(tagger::upgrade_write(file));
 
     let k = &mut hypermelon::tools::upgrade_write(file);
     let mut w = hypermelon::elem::ElemWrite::new(k);
@@ -32,9 +30,11 @@ fn foo<P: AsRef<Path>>(base: P) -> std::fmt::Result {
         .build(|w| {
             let style = build::elem("style").append(include_str!("github-markdown.css"));
 
-            let style2 = poloto::render::Theme::dark();
-                
-            let style = style.chain(style2);
+            let style2=build::elem("style")
+                .append(poloto::render::Theme::dark().get_str())
+                .append(".poloto_scatter{stroke-width:3}");
+
+            let style=style.chain(style2);
 
             w.render(style)?;
 
@@ -54,29 +54,29 @@ fn foo<P: AsRef<Path>>(base: P) -> std::fmt::Result {
 }
 
 pub fn handle(emp: &mut Html, man: &mut DnumManager) -> std::fmt::Result {
-    // colfind::theory(emp, man)?;
+    colfind::theory(emp, man)?;
 
-    // colfind::bench(emp)?;
-    // colfind::bench_grow(emp)?;
-    // colfind::theory_grow(emp, man)?;
-    // best_height::bench(emp)?;
-    // best_height::theory(emp, man)?;
-    // best_height::optimal(emp)?;
-    // levels::bench(emp)?;
-    // levels::theory(emp, man)?;
-    // cached_pairs::bench(emp)?;
+    colfind::bench(emp)?;
+    colfind::bench_grow(emp)?;
+    colfind::theory_grow(emp, man)?;
+    best_height::bench(emp)?;
+    best_height::theory(emp, man)?;
+    best_height::optimal(emp)?;
+    levels::bench(emp)?;
+    levels::theory(emp, man)?;
+    cached_pairs::bench(emp)?;
     float_vs_integer::bench(emp)?;
 
-    // rebal_vs_query::bench(emp)?;
-    // rebal_vs_query::theory(emp, man)?;
+    rebal_vs_query::bench(emp)?;
+    rebal_vs_query::theory(emp, man)?;
 
-    // spiral::handle_visualize(emp)?;
-    // spiral::handle_grow(emp)?;
-    // spiral::num_intersection(emp)?;
+    spiral::handle_visualize(emp)?;
+    spiral::handle_grow(emp)?;
+    spiral::num_intersection(emp)?;
 
-    // layout::bench(emp)?;
+    layout::bench(emp)?;
 
-    // // TODO add back
+    // TODO add back
     // par_tuner::bench_par(emp)?;
     // par_tuner::best_seq_fallback_rebal(emp)?;
     // par_tuner::best_seq_fallback_query(emp)?;
@@ -122,27 +122,13 @@ impl Disper for Custom {
         plot: hypermelon::elem::DynElem,
         description: &str,
     ) -> std::fmt::Result {
-        //let dd = dim;
-        //let svg_width = 380.0;
-        //TODO remove this kind of thing?
-        //let hh = simple_theme::determine_height_from_width(dd, svg_width);
 
         let div=build::elem("div").with(("style","max-width:400px;width:100%;background:#262626;margin:5px;padding:5px;word-break: normal;white-space: normal;border-radius:10px"));
 
-
-        //svg class="poloto" width="100%" viewBox="0 0 800 500" xmlns="http://www.w3.org/2000/svg">
-        //let header = poloto::header().with(("width", "100%"));
         let header=build::elem("svg").with(attrs!(("class","poloto"),("width","100%"),("viewBox","0 0 800 500"),("xmlns","http://www.w3.org/2000/svg")));
-
-        //let style = build::elem("style").append(".poloto_line{stroke-dasharray:2;stroke-width:2;}");
 
         let all = header.append(plot);
         let all = div.append(all);
-
-        // let all = header
-        //     .append(div)
-        //     .append(style)
-        //     .append(plot);
 
         let parser = pulldown_cmark::Parser::new(description);
         let mut s = String::new();
@@ -153,39 +139,7 @@ impl Disper for Custom {
             .append(build::raw_escapable(s));
 
         let all = all.append(text);
-        //TODO return elem instead of writing
+        
         hypermelon::render(all, w)
     }
 }
-
-pub const MY_CONFIG: &str = ".poloto{\
-    stroke-linecap:round;\
-    stroke-linejoin:round;\
-    font-family:Roboto,sans-serif;\
-    font-size:16px;\
-    }\
-    .poloto_background{fill:rgba(0,0,0,0);}\
-    .poloto_scatter{stroke-width:7}\
-    .poloto_tick_line{stroke:dimgray;stroke-width:0.5}\
-    .poloto_line{stroke-width:2}\
-    .poloto_text{fill: white;}\
-    .poloto_axis_lines{stroke: white;stroke-width:3;fill:none;stroke-dasharray:none}\
-    .poloto_title{font-size:24px;dominant-baseline:start;text-anchor:middle;}\
-    .poloto_xname{font-size:24px;dominant-baseline:start;text-anchor:middle;}\
-    .poloto_yname{font-size:24px;dominant-baseline:start;text-anchor:middle;}\
-    .poloto0stroke{stroke:blue;}\
-    .poloto1stroke{stroke:red;}\
-    .poloto2stroke{stroke:green;}\
-    .poloto3stroke{stroke:gold;}\
-    .poloto4stroke{stroke:aqua;}\
-    .poloto5stroke{stroke:lime;}\
-    .poloto6stroke{stroke:orange;}\
-    .poloto7stroke{stroke:chocolate;}\
-    .poloto0fill{fill:blue;}\
-    .poloto1fill{fill:red;}\
-    .poloto2fill{fill:green;}\
-    .poloto3fill{fill:gold;}\
-    .poloto4fill{fill:aqua;}\
-    .poloto5fill{fill:lime;}\
-    .poloto6fill{fill:orange;}\
-    .poloto7fill{fill:chocolate;}";
